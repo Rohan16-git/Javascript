1db7da4c4e1e94781bb15c3138fc0db806105b81
Rohan16-git <roh@angithub16>
2024-11-07 14:43:10 +0530

    new

---
+++ view.js
@@ -0,0 +1,24 @@
+let user = "";
+
+switch (user) {
+  case "I need to know your name":
+    console.log("My name is Rohan2.0");
+    break;
+  case "How is the day today":
+    console.log("Pleasent as always");
+    break;
+
+  case "will you be my friend":
+    console.log("Yes I will be your frined");
+    break;
+  case "Play a song":
+    console.log("which one like romantic,sad,hipop?");
+    break;
+    case "":
+        console.log("");
+        break;
+
+  default:
+    console.log("how can i help you");
+    break;
+}
